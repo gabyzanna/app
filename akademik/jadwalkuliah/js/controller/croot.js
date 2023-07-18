3a6d86fe852a6ca48058d7e9ddfb77143b273c0f
Rolly Maulana Awangga <rolly@awang.ga>
2023-07-18 12:53:53 +0700

    br

--- akademik/jadwalkuliah/js/controller/croot.js
+++ akademik/jadwalkuliah/js/controller/croot.js
@@ -1,20 +1,20 @@
 import { setInner, addInner } from "https://jscroot.github.io/element/croot.js";
 import {get} from "https://jscroot.github.io/api/croot.js"; 
-import { rowtabel } from "./view/tabel.js";
+import { rowtabel } from "../template/tabel.js";
+import { URL } from "../config/url.js";
 setInner("namadivisi","Data Mahasiswa");
-get("https://jadwal-0fc5e0bbd0c5.herokuapp.com/mhs", tabelContent);
-
-var isi = {};
+get(URL, tabelContent);
 
 function tabelContent(result){
     userTable(result);
-    isi = result;
 }
 
 function userTable(jsonParse){
 
     jsonParse.forEach((element, index) => {
-    let row = rowtabel.replace("#namamhs#", element.namamhs).replace("#kelas#", element.kelas).replace("#prodi#", element.prodi);
+    let row = rowtabel.replace("#namamhs#", element.namamhs).
+    replace("#kelas#", element.kelas).
+    replace("#prodi#", element.prodi);
     console.log(element._id);
     addInner("demo",row);
     });
